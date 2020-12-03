20c8a4159049b0295bda875c49cabb9253124e2f
Marton Gaspardy <gaspy89@gmail.com>
2020-12-03 16:22:11 +0100

    implemented map based services. closes #6

---
+++ pet-clinic-data/src/main/java/com/gaspy/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.gaspy.sfgpetclinic.services.map;
+
+import com.gaspy.sfgpetclinic.model.Pet;
+import com.gaspy.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+}
