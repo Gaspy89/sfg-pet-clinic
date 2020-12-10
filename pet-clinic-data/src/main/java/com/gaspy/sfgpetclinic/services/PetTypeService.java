4106619de34e9115aaa34b4a73e2bfe239d1f32a
Marton Gaspardy <gaspy89@gmail.com>
2020-12-10 13:29:13 +0100

    implemented pettype service

---
+++ pet-clinic-data/src/main/java/com/gaspy/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.gaspy.sfgpetclinic.services;
+
+import com.gaspy.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/gaspy/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,39 @@
+package com.gaspy.sfgpetclinic.services.map;
+
+import com.gaspy.sfgpetclinic.model.Pet;
+import com.gaspy.sfgpetclinic.model.PetType;
+import com.gaspy.sfgpetclinic.services.PetService;
+import com.gaspy.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+}
