e02278796831c20ac60f2e8046fa8ad9c135a5ef
Marton Gaspardy <gaspy89@gmail.com>
2020-12-15 17:38:14 +0100

    updated vet entity to use jpa

--- pet-clinic-data/src/main/java/com/gaspy/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/gaspy/sfgpetclinic/model/Owner.java
@@ -1,9 +1,6 @@
 package com.gaspy.sfgpetclinic.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,8 +8,47 @@ import java.util.Set;
 @Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "telephone")
+    private String telephone;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String getTelephone() {
+        return telephone;
+    }
+
+    @Override
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
--- pet-clinic-data/src/main/java/com/gaspy/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/gaspy/sfgpetclinic/model/Person.java
@@ -11,15 +11,6 @@ public class Person extends BaseEntity {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "address")
-    private String address;
-
-    @Column(name = "city")
-    private String city;
-
-    @Column(name = "telephone")
-    private String telephone;
-
     public String getFirstName() {
         return firstName;
     }
@@ -36,27 +27,4 @@ public class Person extends BaseEntity {
         this.lastName = lastName;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
 }
